24c917f3f531eadee42f2850fac2690a22c78d37
zhoujianhang <942125780@qq.com>
2021-12-03 16:49:38 +0800

    2021-12-03-16:48-v3.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hardware-check
 hardware-check
+
+# 不同的操作系统对应对应不同的文件夹，因为命令不一样
+# 根据实际硬件检测信息进行调整
+# 可扩展centos等其他硬件检测
--- hardware_detection_script.sh
+++ hardware_detection_script.sh
@@ -3,15 +3,16 @@
 function hardware_cpu_mem_network_NVIDIA() {
     #根据实际情况改动hosts，根据ansible分组进行区分操作系统这里默认使用all
     #cpu检测输出至文件
-    ansible all -m shell -a 'lscpu |grep -aE "Model name|Socket" |sed s/[[:space:]]//g' >> cpu_info.txt
+    ansible all -m shell -a 'lscpu |grep -aE "Model name|Socket" |sed s/[[:space:]]//g' >> ./result/cpu_info_`date +'%Y_%m_%d_%H:%M:%S'`.txt
     #总内存检测输出至文件
-    ansible all -m shell -a 'free -hm |sed s/[[:space:]]//g|grep Mem| cut -d 'G' -f 1' >> total_memory_info.txt
+    ansible all -m shell -a 'free -hm |sed s/[[:space:]]//g|grep Mem| cut -d 'G' -f 1' >> ./result/total_memory_info_`date +'%Y_%m_%d_%H:%M:%S'`.txt
     #网卡检测输出至文件
-    ansible all -m shell -a 'lshw -C net|grep -aE "logical name" |awk "{print \$3}" |xargs -I "{}" ethtool {}|grep -aE "Settings|Speed"' >> network_card_info.txt
+    ansible all -m shell -a 'lshw -C net|grep -aE "logical name" |awk "{print \$3}" |xargs -I "{}" ethtool {}|grep -aE "Settings|Speed"' >> ./result/network_card_info_`date +'%Y_%m_%d_%H:%M:%S'`.txt
     #收集显卡信息，有几张显卡
-    ansible all -m shell -a 'nvidia-smi -q|grep -aE "Attached GPUs"|sed s/[[:space:]]//g' >> numb_gpu_info.txt
+    ansible all -m shell -a 'nvidia-smi -q|grep -aE "Attached GPUs"|sed s/[[:space:]]//g' >> ./result/numb_gpu_info_`date +'%Y_%m_%d_%H:%M:%S'`.txt
     #收集nvidia-smi结果输出
-    ansible all -m shell -a 'nvidia-smi' >> GPU_info.txt
+    ansible all -m shell -a 'nvidia-smi' >> ./result/GPU_info_`date +'%Y_%m_%d_%H:%M:%S'`.txt
+    echo "---收集完成"
 }
 
 #ubuntu配置DNS
@@ -25,15 +26,15 @@ function set_DNS() {
 }
 
 #检测是否安装显卡驱动并且调用收集函数
-function  start_collect() {
-     if ! type nvidia-smi; then
-          echo "---未安装驱动，请手动安装命令：ansible-playbook install_GPU_driver"
-     else
-          echo "---已经安装显卡驱动"
-          echo "---开始收集cpu，内存，网卡，显卡信息到文件cpu_info.txt，total_memory_info.txt，network_card_info.txt，numb_gpu_info.txt，GPU_info.txt"
-          #收集
-          hardware_cpu_mem_network_NVIDIA
-     fi
+function start_collect() {
+     #开始安装显卡驱动
+     echo "---开始安装显卡驱动"
+     ansible-playbook ./ubuntu/install_GPU_driver.yml
+     echo "---开始收集cpu，内存，网卡，显卡信息到文件cpu_info.txt，total_memory_info.txt，network_card_info.txt，numb_gpu_info.txt，GPU_info.txt"
+     hardware_cpu_mem_network_NVIDIA
+     echo "---删除显卡驱动"
+     ansible-playbook ./ubuntu/remove_GPU_driver.yml
+     echo "---执行完毕---"
 }
 
 function install_fail() {
@@ -53,8 +54,8 @@ function analysis_result() {
 
 hostname=$(hostnamectl |grep -a 'Operating System'|awk '{print tolower($3)}')
 if [ $hostname = 'ubuntu' ]; then
-     echo "---操作系统为：${hostname}"
-     echo "---开始检测是否安装ansible"
+     echo "---宿主机操作系统为：${hostname}"
+     echo "---开始检测是否安装：ansible"
      if ! type ansible; then
           echo "---未安装ansible，现在开始安装"
           #-c表示次数，-w表示time out时间，单位秒
@@ -66,8 +67,6 @@ if [ $hostname = 'ubuntu' ]; then
                     install_fail
                else
                     install_success_collect
-                    echo "---分析数据"
-                    echo "删除驱动"
                fi
           else
                echo "---网络不正常,配置DNS，安装"
@@ -76,15 +75,11 @@ if [ $hostname = 'ubuntu' ]; then
                     install_fail
                else
                     install_success_collect
-                    echo "---分析数据"
-                    echo "删除驱动"
                fi
           fi
      else
           echo "---ansible已安装成功"
           install_success_collect
-          echo "---分析数据"
-          echo "删除驱动"
      fi
 
 elif [ $hostname = 'centos' ]; then
